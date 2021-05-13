854057bc1fd329a718d8edf00f33dcd6022df689
KhanhNguyen4999 <nguyenngocminhkhanh@gmail.com>
2021-05-13 11:10:55 +0700

    Update and push code to reconstruct repo

---
+++ routes/schedule.route.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var router = express.Router();
+
+var controller = require("../controller/schedule.controller");
+
+router.get("/create", controller.createSchedule);
+
+router.post("/PostData", controller.schedulePostData);
+
+// router.put("/updateReserved", controller.undateReserved);
+
+router.get("/data", controller.getScheduleData);
+
+router.get("/data/:id", controller.getScheduleDataByID);
+
+module.exports = router;
